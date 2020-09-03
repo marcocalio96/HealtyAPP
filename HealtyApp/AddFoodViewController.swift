6c02914abb45d71b84c28e8200f130ef8dee34d3
marcocalio96 <caliomarco961@gmail.com>
2020-09-04 00:24:40 +0200

    fature add foods

---
+++ HealtyApp/AddFoodViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddFoodViewController.swift
+//  HealtyApp
+//
+//  Created by Marco Caliò on 03/09/2020.
+//  Copyright © 2020 Marco Caliò. All rights reserved.
+//
+
+import UIKit
+
+class AddFoodViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ HealtyApp/Food.swift
@@ -0,0 +1,9 @@
+//
+//  Food.swift
+//  HealtyApp
+//
+//  Created by Marco Caliò on 03/09/2020.
+//  Copyright © 2020 Marco Caliò. All rights reserved.
+//
+
+import Foundation
